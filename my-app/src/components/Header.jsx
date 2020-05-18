f2025eb76854c95cc5edb6e42bfadca6abc3f96e
AlexandrHeroCoud <50858752+AlexandrHeroCoud@users.noreply.github.com>
2020-05-18 23:41:45 +0300

    Initial commit

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () =>{
+    return(
+        <header className={'header'}>
+            <img src="https://www.pngfind.com/pngs/m/439-4395607_model-image-t-shirt-mr-robot-fsociety-hd.png" alt=""/>
+        </header>
+    )
+}
+export default Header
