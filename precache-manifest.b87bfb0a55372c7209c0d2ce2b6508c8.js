78c758449db79e3a03d757b405f7919746e8ad86
AlexandrHeroCoud <50858752+AlexandrHeroCoud@users.noreply.github.com>
2020-08-01 02:41:34 +0300

    Updates

---
+++ precache-manifest.b87bfb0a55372c7209c0d2ce2b6508c8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399c54df0611ec62e11774aaa097ce08",
+    "url": "/01-react/index.html"
+  },
+  {
+    "revision": "5b7e274eed868d9dd4fa",
+    "url": "/01-react/static/css/3.d751d7cb.chunk.css"
+  },
+  {
+    "revision": "fd8672da8dc04d925206",
+    "url": "/01-react/static/css/4.4c4f1de9.chunk.css"
+  },
+  {
+    "revision": "ed73d73ed0f2429172c1",
+    "url": "/01-react/static/css/main.e96727cc.chunk.css"
+  },
+  {
+    "revision": "b1e49207dc55dc2cae33",
+    "url": "/01-react/static/js/2.561cbc84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/01-react/static/js/2.561cbc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7e274eed868d9dd4fa",
+    "url": "/01-react/static/js/3.411c2940.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/01-react/static/js/3.411c2940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8672da8dc04d925206",
+    "url": "/01-react/static/js/4.f1fe7693.chunk.js"
+  },
+  {
+    "revision": "ed73d73ed0f2429172c1",
+    "url": "/01-react/static/js/main.ab68e610.chunk.js"
+  },
+  {
+    "revision": "e82cc5dc834e40f653cc",
+    "url": "/01-react/static/js/runtime-main.99c62a39.js"
+  }
+]);
